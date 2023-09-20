101bcf46a913edc7e29283b83430c0ee65912fbc
JanPazout <144204850+JanPazout@users.noreply.github.com>
2023-09-20 10:49:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zlomek z1 = new Zlomek(1, 7);
+            Zlomek z2 = new Zlomek(2, 9);
+            Zlomek z3 = Zlomek.scitani(z1, z2);
+            Console.WriteLine(z3);
+            Zlomek z4 = Zlomek.odcitani(z1, z2);
+            Console.WriteLine(z4);
+            Zlomek z5 = Zlomek.nasobeni(z1, z2);
+            Console.WriteLine(z5);
+            Zlomek z6 = Zlomek.deleni(z1, z2);
+            Console.WriteLine(z6);
+        }
+    }
+}
---
+++ Zlomek.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV
+{
+    internal class Zlomek
+    {
+        public int citatel;
+        public int jmenovatel;
+        private int v;
+
+        public int Citatel
+        {
+            get { return citatel; }
+            set { citatel = value; }
+        }
+
+        public int Jmenovatel
+        {
+
+            get { return jmenovatel; }
+            set { jmenovatel = value; }
+        }
+        public Zlomek(int citatel, int jmenovatel)
+        {
+            this.citatel = citatel;
+            this.jmenovatel = jmenovatel;
+        }
+
+        public Zlomek(int v)
+        {
+            this.v = v;
+        }
+
+        public static Zlomek create(int citatel, int jmenovatel)
+        {
+            if (jmenovatel == 0)
+            {
+                throw new Exception();
+            }
+            return new Zlomek(citatel, jmenovatel);
+        }
+
+
+
+          public static Zlomek scitani(Zlomek z, Zlomek z2)
+          {
+              Zlomek zlomek = new Zlomek(z.citatel * z2.jmenovatel + z2.citatel * z.jmenovatel, z.jmenovatel * z2.jmenovatel);
+              return zlomek;
+          }
+        public static Zlomek odcitani(Zlomek z, Zlomek z2)
+        {
+            Zlomek zlomek = new Zlomek(z.citatel * z2.jmenovatel - z2.citatel * z.jmenovatel, z.jmenovatel * z2.jmenovatel);
+            return zlomek;  
+        }
+        public static Zlomek nasobeni(Zlomek z, Zlomek z2)
+        {
+            Zlomek zlomek = new Zlomek(z.citatel * z2.citatel, z.jmenovatel * z2.jmenovatel);
+            return zlomek;
+        }
+        public static Zlomek deleni(Zlomek z, Zlomek z2)
+        {
+            Zlomek zlomek = new Zlomek(z.citatel * z2.jmenovatel, z.jmenovatel * z2.citatel);
+            return zlomek;
+        }
+        public override string ToString()
+        {
+            return citatel + "/" + jmenovatel;
+        }
+        public void Kraceni()
+        {
+            citatel /= NSD(citatel, jmenovatel);
+            jmenovatel /= NSD(citatel, jmenovatel);
+        }
+        public static int NSD(int a, int b)
+        {
+            int c;
+            while(b != 0)
+            {
+                c = b;
+                b = a % b;
+                a = c;
+            }
+            return a;
+        }
+
+
+    }
+}
